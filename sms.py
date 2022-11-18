94962b61308c1829b492ec456d3c5329d44d3413
dipamato <66480235+dipamato@users.noreply.github.com>
2022-11-18 02:51:19 -0500

    otra con el environment

--- sms.py
+++ sms.py
@@ -67,5 +67,5 @@ def email():
     
 
 if __name__ == "__main__":
-    port = int(os.environment.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0',port=port)
