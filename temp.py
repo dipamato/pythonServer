77a2626ae24ebe6e3b28ec3521a522b3d4581912
dipamato <66480235+dipamato@users.noreply.github.com>
2022-11-18 02:44:59 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -9,6 +9,6 @@ import os
 
 os.environ['TWILIO_ACCOUNT_SID']="AC8d226d4e4080d1ee5945c1cd6fdeb82a"
 os.environ['TWILIO_AUTH_TOKEN']="8b39acaab5949880c5d3aee3ec640541"
-os.environ['SENDGRID_API_KEY']="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"
+os.environ['SENDGRID_API_KEY']="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"
 
 os.environ["Test"]="Prueba de variable de entorno"
